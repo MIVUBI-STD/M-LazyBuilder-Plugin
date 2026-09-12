b5616420f532aa06331cf2e8ad18e728c8297b4a
halokaryamedia-source <halokaryamedia@gmail.com>
2026-09-12 23:48:30 +0700

    feat: add Worlds page code-behind

---
+++ apps/lazybuilder-desktop/Views/WorldsView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace HaloKaryaMedia.LazyBuilder.Desktop.Views;
+
+public partial class WorldsView : UserControl
+{
+    public WorldsView()
+    {
+        InitializeComponent();
+    }
+}
