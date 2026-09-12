f9341824de03ea035b28bad455aec94f5aa3cbfe
halokaryamedia-source <halokaryamedia@gmail.com>
2026-09-13 01:51:43 +0700

    feat: reserve exclusive backup world operation

--- modules/world-manager/src/main/java/com/halokaryamedia/lazybuilder/world/application/WorldOperationType.java
+++ modules/world-manager/src/main/java/com/halokaryamedia/lazybuilder/world/application/WorldOperationType.java
@@ -3,6 +3,7 @@ package com.halokaryamedia.lazybuilder.world.application;
 /** Exclusive request-bound operation types for one managed world. */
 public enum WorldOperationType {
     CLONE,
+    BACKUP,
     ARCHIVE,
     DELETE,
     IMPORT,
