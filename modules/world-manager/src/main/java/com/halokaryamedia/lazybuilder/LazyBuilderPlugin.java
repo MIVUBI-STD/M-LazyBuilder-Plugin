5bfd660fed3c5289b38f5290d5d7237d678cb79a
halokaryamedia-source <halokaryamedia@gmail.com>
2026-09-13 02:36:11 +0700

    refactor: reduce legacy LazyBuilderPlugin to compatibility base

--- modules/world-manager/src/main/java/com/halokaryamedia/lazybuilder/LazyBuilderPlugin.java
+++ modules/world-manager/src/main/java/com/halokaryamedia/lazybuilder/LazyBuilderPlugin.java
@@ -1,107 +1,14 @@
 package com.halokaryamedia.lazybuilder;
 
-import com.halokaryamedia.lazybuilder.world.WorldManager;
-import com.halokaryamedia.lazybuilder.world.paper.LocalControlImportUploadService;
-import com.halokaryamedia.lazybuilder.world.paper.PaperLocalControlServer;
-import com.halokaryamedia.lazybuilder.world.paper.PaperMapActionPayloadAdapter;
-import com.halokaryamedia.lazybuilder.world.paper.PaperTransferPayloadAdapter;
-import com.halokaryamedia.lazybuilder.world.paper.PaperWorldControlPayloadAdapter;
-import com.halokaryamedia.lazybuilder.world.task.WorldTaskRegistry;
-import com.halokaryamedia.lazybuilder.world.task.WorldTaskRunner;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * Internal World-Manager bootstrap base retained temporarily while downstream adapters are
- * decoupled from the historical class name. Paper loads {@link WorldManagerPlugin}.
+ * Compatibility base for adapters that still accept the historical plugin type.
+ *
+ * <p>Lifecycle ownership has moved to {@link WorldManagerPlugin}. This class intentionally
+ * contains no bootstrap state or behavior and can be removed once remaining adapter
+ * constructor types are generalized to {@link JavaPlugin}.</p>
  */
-@Deprecated(forRemoval = false)
-public class LazyBuilderPlugin extends JavaPlugin {
-    private WorldManager worldManager;
-    private WorldTaskRegistry worldTaskRegistry;
-    private WorldTaskRunner worldTaskRunner;
-    private PaperTransferPayloadAdapter transferPayloadAdapter;
-    private PaperMapActionPayloadAdapter mapActionPayloadAdapter;
-    private PaperWorldControlPayloadAdapter worldControlPayloadAdapter;
-    private PaperLocalControlServer localControlServer;
-
-    @Override
-    public void onEnable() {
-        saveDefaultConfig();
-        this.worldManager = new WorldManager(this);
-        this.worldTaskRegistry = new WorldTaskRegistry();
-        this.worldTaskRunner = new WorldTaskRunner(worldTaskRegistry);
-        this.worldManager.start();
-
-        this.transferPayloadAdapter = new PaperTransferPayloadAdapter(this, worldManager.transferSessionService());
-        this.transferPayloadAdapter.start();
-
-        this.mapActionPayloadAdapter = new PaperMapActionPayloadAdapter(
-                this,
-                worldManager.worldRegistry(),
-                worldManager.worldLocationTeleportService(),
-                worldManager.worldExportService()
-        );
-        this.mapActionPayloadAdapter.start();
-
-        this.worldControlPayloadAdapter = new PaperWorldControlPayloadAdapter(
-                this,
-                worldManager.worldRegistry(),
-                worldManager.worldCreationService(),
-                worldManager.worldRuntimeService(),
-                worldManager.worldTeleportService(),
-                worldManager.worldLifecycleService(),
-                worldManager.worldCloneService(),
-                worldManager.worldDeleteService(),
-                worldManager.worldSettingsService(),
-                worldManager.worldExportService(),
-                worldManager.worldImportService()
-        );
-        this.worldControlPayloadAdapter.start();
-
-        LocalControlImportUploadService importUploads = new LocalControlImportUploadService(
-                worldManager.transferSessionService(),
-                worldManager.transferPolicy()
-        );
-        this.localControlServer = new PaperLocalControlServer(
-                this,
-                worldManager.worldRegistry(),
-                worldManager.worldRuntimeService(),
-                worldManager.worldCreationService(),
-                worldManager.worldSettingsService(),
-                worldManager.worldLifecycleService(),
-                worldManager.worldCloneService(),
-                worldManager.worldBackupService(),
-                worldManager.worldExportService(),
-                worldManager.worldImportService(),
-                worldManager.worldDeleteService(),
-                importUploads,
-                worldTaskRegistry,
-                worldTaskRunner
-        );
-        this.localControlServer.start();
-        getLogger().info("World-Manager enabled.");
-    }
-
-    @Override
-    public void onDisable() {
-        if (localControlServer != null) {
-            localControlServer.stop();
-        }
-        if (worldTaskRunner != null) {
-            worldTaskRunner.close();
-        }
-        if (worldControlPayloadAdapter != null) {
-            worldControlPayloadAdapter.stop();
-        }
-        if (mapActionPayloadAdapter != null) {
-            mapActionPayloadAdapter.stop();
-        }
-        if (transferPayloadAdapter != null) {
-            transferPayloadAdapter.stop();
-        }
-        if (worldManager != null) {
-            worldManager.stop();
-        }
-        getLogger().info("World-Manager disabled.");
-    }
+@Deprecated(forRemoval = true)
+public abstract class LazyBuilderPlugin extends JavaPlugin {
 }
