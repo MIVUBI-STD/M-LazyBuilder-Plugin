e7ffa4fb1eb9515e28befeb57b1a425af777a007
halokaryamedia-source <halokaryamedia@gmail.com>
2026-09-14 04:02:04 +0700

    world: rename copy profile to duplicate

--- modules/world-manager/src/main/java/com/halokaryamedia/lazybuilder/world/files/WorldCopyProfile.java
+++ modules/world-manager/src/main/java/com/halokaryamedia/lazybuilder/world/files/WorldCopyProfile.java
@@ -3,5 +3,5 @@ package com.halokaryamedia.lazybuilder.world.files;
 /** Copy intent controls which identity/player-local files are retained. */
 public enum WorldCopyProfile {
     SNAPSHOT,
-    CLONE
+    DUPLICATE
 }
