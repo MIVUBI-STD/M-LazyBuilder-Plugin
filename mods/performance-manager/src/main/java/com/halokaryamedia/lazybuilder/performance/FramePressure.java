c90b9e0116715d540be623699e6e89d2ac528769
halokaryamedia-source <halokaryamedia@gmail.com>
2026-09-15 02:00:41 +0700

    refactor(performance): add frame pressure model

---
+++ mods/performance-manager/src/main/java/com/halokaryamedia/lazybuilder/performance/FramePressure.java
@@ -0,0 +1,8 @@
+package com.halokaryamedia.lazybuilder.performance;
+
+/** Lightweight internal signal describing current client frame pressure. */
+public enum FramePressure {
+    NORMAL,
+    ELEVATED,
+    HEAVY
+}
