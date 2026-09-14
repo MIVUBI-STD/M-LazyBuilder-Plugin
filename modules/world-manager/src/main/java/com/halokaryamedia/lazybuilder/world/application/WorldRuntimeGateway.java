068304dcf28e4295bb4aadb93772b0a40efe1496
halokaryamedia-source <halokaryamedia@gmail.com>
2026-09-14 11:05:51 +0700

    Keep player presence at runtime service composition boundary

--- modules/world-manager/src/main/java/com/halokaryamedia/lazybuilder/world/application/WorldRuntimeGateway.java
+++ modules/world-manager/src/main/java/com/halokaryamedia/lazybuilder/world/application/WorldRuntimeGateway.java
@@ -12,11 +12,6 @@ public interface WorldRuntimeGateway {
 
     boolean isLoaded(WorldRecord world);
 
-    /** True only when one or more players are currently inside the managed world. */
-    default boolean hasPlayers(WorldRecord world) {
-        return false;
-    }
-
     void loadWorld(WorldRecord world);
 
     void unloadWorld(WorldRecord world);
