f2d0e1c8b49e9ecada83ae00f7c7204a55b2a790
MIVUBI-STD <mivubiteam@gmail.com>
2026-09-18 23:13:31 +0700

    refactor(builder): consolidate placement all-constraint helper

--- mods/builder-utilities/src/main/java/com/halokaryamedia/lazybuilder/builder/placement/PlacementConstraint.java
+++ mods/builder-utilities/src/main/java/com/halokaryamedia/lazybuilder/builder/placement/PlacementConstraint.java
@@ -3,8 +3,4 @@ package com.halokaryamedia.lazybuilder.builder.placement;
 @FunctionalInterface
 public interface PlacementConstraint {
     boolean test(PlacementPoint point);
-
-    static PlacementConstraint all() {
-        return point -> true;
-    }
 }
