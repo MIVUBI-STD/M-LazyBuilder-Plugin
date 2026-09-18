e5392cdd3158713b6aaf25f95912172b33ad1788
MIVUBI-STD <mivubiteam@gmail.com>
2026-09-18 23:33:53 +0700

    refactor(builder): remove superseded direct mutation executor

--- mods/builder-utilities/src/main/java/com/halokaryamedia/lazybuilder/builder/mutation/WorldBlockMutationTarget.java
+++
@@ -1,5 +0,0 @@
-package com.halokaryamedia.lazybuilder.builder.mutation;
-
-public interface WorldBlockMutationTarget extends WorldBlockStateSource {
-    void writeBlockState(int worldX, int y, int worldZ, String blockState);
-}
