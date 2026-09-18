7e854762c5b3d9be5c918ee8ff5203396ea468e0
MIVUBI-STD <mivubiteam@gmail.com>
2026-09-18 23:29:00 +0700

    refactor(builder): remove orphan compatibility primitives

--- mods/builder-utilities/src/main/java/com/halokaryamedia/lazybuilder/builder/mutation/ExtensionReconciliationState.java
+++
@@ -1,9 +0,0 @@
-package com.halokaryamedia.lazybuilder.builder.mutation;
-
-public enum ExtensionReconciliationState {
-    EMPTY,
-    NOT_APPLIED,
-    FULLY_APPLIED,
-    PARTIALLY_APPLIED,
-    CONFLICT
-}
