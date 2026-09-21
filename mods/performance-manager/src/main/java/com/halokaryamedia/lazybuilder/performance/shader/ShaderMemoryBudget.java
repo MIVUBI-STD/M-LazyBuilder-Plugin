b65491fbd7450fe7f4ace465f90ca73f9f4b61c8
MIVUBI-STD <mivubiteam@gmail.com>
2026-09-21 12:42:15 +0700

    refactor(shader): remove stale auxiliary memory constant

--- mods/performance-manager/src/main/java/com/halokaryamedia/lazybuilder/performance/shader/ShaderMemoryBudget.java
+++ mods/performance-manager/src/main/java/com/halokaryamedia/lazybuilder/performance/shader/ShaderMemoryBudget.java
@@ -9,7 +9,6 @@ package com.halokaryamedia.lazybuilder.performance.shader;
  */
 public final class ShaderMemoryBudget {
     private static final long MIB = 1024L * 1024L;
-    private static final long DEFAULT_MAX_AUXILIARY_BYTES = 512L * MIB;
     private static final long MAX_AUXILIARY_BYTES = configuredLimitBytes();
     private static final long RGBA16F_BYTES_PER_PIXEL = 8L;
     private static final long DEPTH32F_BYTES_PER_PIXEL = 4L;
